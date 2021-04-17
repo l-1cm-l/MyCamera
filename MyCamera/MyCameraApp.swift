9ca98a10dc46fa349cca6b1eb012aec850dd8579
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-04-18 07:30:33 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by administrator on 2021/04/18.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
